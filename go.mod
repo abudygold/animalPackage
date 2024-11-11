bb6067f533127b40ac87ac4ffaf91b4dfed72a9f
abudy.gold <abudy.gold@yahoo.com>
2024-11-11 10:45:40 +0700

    init project

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abudygold/animalPackage
 
 go 1.23.0
+
+require github.com/abudygold/puppy v1.0.0
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"runtime"
+
+	puppyLib "github.com/abudygold/puppy"
 )
 
 func main() {
-	fmt.Println("Hello Gophers Again!")
+	s1 := puppyLib.Bark()
+	s2 := puppyLib.Barks()
+	s3 := puppyLib.WhenGrownUp("Guguk")
+
 	fmt.Println(runtime.GOOS)
 	fmt.Println(runtime.GOARCH)
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(puppyLib.Bark())
+	fmt.Println(puppyLib.Barks())
+	fmt.Println(puppyLib.WhenGrownUp("Guguk"))
 }
